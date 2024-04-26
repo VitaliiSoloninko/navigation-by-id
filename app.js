8f6660459e50b5e528c34c301ac5818643da4fc9
22051988Di <vitalii.soloninko@gmail.com>
2024-04-26 17:11:06 +0200

    version 1.0.0	JavaScript

---
+++ app.js
@@ -0,0 +1,14 @@
+// anchors - якоря
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+	anchor.addEventListener('click', function (event) {
+		event.preventDefault() // відмінили стандарте повеение кнопки, зараз вона не працює
+		const blockID = anchor.getAttribute('href')
+		document.querySelector('' + blockID).scrollIntoView({
+			behavior: 'smooth',
+			block: 'start',
+		})
+	})
+}
